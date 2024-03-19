3edc9a99c97dda0a3ced43bf5b4b376ca6fb66e3
Guilherme Machado <guilhermesvmachado@gmail.com>
2024-03-19 12:06:12 -0300

    Sending JavaFX files

---
+++ Projects-JavaFX/HoraSistema/src/horasistema/HoraSistema.java
@@ -0,0 +1,11 @@
+package horasistema;
+
+import java.util.Date;
+
+public class HoraSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do relógio é:");
+        System.out.println(relogio.toString());
+    }
+}
